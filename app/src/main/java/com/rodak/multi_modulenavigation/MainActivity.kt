a43a9e37e5d2650bfbfa6a0c3c319c66b51cdddc
danielr <daniel.rodak@untitledkingdom.com>
2019-08-07 21:16:59 +0200

    Initial commit

---
+++ app/src/main/java/com/rodak/multi_modulenavigation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rodak.multi_modulenavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

e6aeb6504b9bb30ba48d4f8299064a4b74b0f373
danielr <daniel.rodak@untitledkingdom.com>
2019-08-09 15:56:01 +0200

    #02 Basic implementation of the first feature

---
+++ firstFeature/src/main/java/com/rodak/firstfeature/FeatureActivity.kt
@@ -0,0 +1,12 @@
+package com.rodak.firstfeature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FeatureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feature)
+    }
+}

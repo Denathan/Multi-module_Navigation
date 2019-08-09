b12e3879fa6402a75f1d99e2e3ecf1fdb00a41c0
danielr <daniel.rodak@untitledkingdom.com>
2019-08-09 16:10:42 +0200

    #03 Added button on the main screen

--- app/src/main/java/com/rodak/multi_modulenavigation/MainActivity.kt
+++ app/src/main/java/com/rodak/multi_modulenavigation/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.rodak.multi_modulenavigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rodak.multi_modulenavigation.extensions.display
 
 class MainActivity : AppCompatActivity() {
+
+    private val defaultTag = "TAG"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MainFragment().display(supportFragmentManager, R.id.fragmentContainer, defaultTag)
     }
 }
---
+++ app/src/main/java/com/rodak/multi_modulenavigation/extensions/FragmentExtensions.kt
@@ -0,0 +1,10 @@
+package com.rodak.multi_modulenavigation.extensions
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
+fun Fragment.display(supportFragmentManager: FragmentManager, containerViewId: Int, fragmentTag: String) {
+    val fragmentTransaction = supportFragmentManager.beginTransaction()
+    fragmentTransaction.replace(containerViewId, this, fragmentTag)
+    fragmentTransaction.commit()
+}
